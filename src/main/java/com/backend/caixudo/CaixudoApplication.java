4499e881827984c393677d1103302f796232a8fc
Joao Lucas <joaolmb.dev@hotmail.com>
2025-04-13 21:50:36 -0300

    primeiro commit

---
+++ src/main/java/com/backend/caixudo/CaixudoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.caixudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/caixudo/CaixudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.caixudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
